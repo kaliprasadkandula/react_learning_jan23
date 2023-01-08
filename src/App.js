6f3eaf493c4e573e9e27f2b17948eefe859b7fad
TSVK Prasad K <kaliprasad4035@gmail.com>
2023-01-08 14:04:43 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+function Component1(props){//Note components starts with capital letters//you know?you can also create components using class
+ return <div className={props.nameForClass}>{props.name}</div>
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here's my best friend's List 
+          <Header/>
+          <Component1 name={"kaka1"} nameForClass={"kaka1Class"}/>
+          <Component1 name={"kaka2"} nameForClass={"kaka2Class"}/>
+          <Component1 name={"kaka3"} nameForClass={"kaka3Class"}/>
+          
+
         </p>
         <a
           className="App-link"
@@ -15,7 +24,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with kaka
         </a>
       </header>
     </div>
